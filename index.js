520eaecfff2f6e8762c343026dfa5d5b6e925ff8
BryanEnriquez1 <bryan.enriquez487@ist17dejulio.edu.ec>
2025-01-08 15:29:30 -0500

    Implementacion del Metodo Get

--- index.js
+++ index.js
@@ -6,7 +6,25 @@ app.use(express.json());
 let datosP = [];
 
 app.all('/Datos/All', (req,res)=>{
-    
+    if (req.method === 'GET') {
+        
+        const idp = parseInt(req.query.idpersona);
+        if (idp) {
+            const persona = datosP.find((p) => p.id === idp);
+            if (!persona) {
+                return res.status(404).json({ error: "Persona no encontrada" });
+            }
+            return res.json(persona);
+        } else {
+            return res.json(datosP);
+        }
+    }else if (req.method === 'DELETE'){
+        
+    }
+
+    res.status(405).json({
+        message: `Método ${req.method} no está implementado para esta ruta.`,
+    });
 })
 
 // Iniciar el servidor
